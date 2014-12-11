0525f2c1bdcfacada49ccaeb432f4369fb348ae1
alexander-daniel <alex.vados@gmail.com>
2014-12-11 11:41:50 -0500

    more tests

--- test/test.js
+++ test/test.js
@@ -13,5 +13,21 @@ thingFactory.once('ready', function () {
                    'inheritance chain correct');
         t.end();
     });
-   
+
+    tape.test('Base - Extended - Super | inheritance with no spec', function (t) {
+        t.plan(1);
+        var SuperState = thingFactory.create('SuperClass');
+        var inheritance = thingFactory.inheritance(SuperState);
+        t.deepEqual(inheritance, [ 'BaseClass', 'ExtendedClass', 'SuperClass' ],
+                   'inheritance chain correct');
+        t.end();
+    });
+
+    tape.test('Foobar Class', function (t) {
+        t.plan(1);
+        var SuperState = thingFactory.create('fooclass');
+        t.equal(null, SuperState, 'should be null, no fooclass')
+        t.end();
+    });
+
 });
